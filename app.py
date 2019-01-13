842f4613e9984fdc0ec183a4e9ec813865ada376
Franklin <franklin.liang@live.com>
2019-01-13 16:12:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
-from flask import Flask, render_template, json, request
+from flask import Flask, render_template, json, request, redirect, session
 from flaskext.mysql import MySQL
 from werkzeug import generate_password_hash, check_password_hash
 
 mysql = MySQL()
 app = Flask(__name__)
+app.secret_key = 'why would I tell you my secret key?'
 
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = ''
-app.config['MYSQL_DATABASE_DB'] = 'BucketList'
+app.config['MYSQL_DATABASE_DB'] = 'bucketlist'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
-
 
 @app.route('/')
 def main():
@@ -20,6 +20,21 @@ def main():
 def showSignUp():
     return render_template('signup.html')
 
+@app.route('/showSignin')
+def showSignin():
+    return render_template('signin.html')
+
+@app.route('/userHome')
+def userHome():
+    if session.get('user'):
+        return render_template('userHome.html')
+    else:
+        return render_template('error.html',error = 'Unauthorized Access')
+
+@app.route('/logout')
+def logout():
+    session.pop('user',None)
+    return redirect('/')
 
 @app.route('/signUp',methods=['POST','GET'])
 def signUp():
@@ -28,6 +43,7 @@ def signUp():
         _email = request.form['inputEmail']
         _password = request.form['inputPassword']
 
+
         if _name and _email and _password:
 
             conn = mysql.connect()
@@ -35,6 +51,7 @@ def signUp():
             _hashed_password = generate_password_hash(_password)
             cursor.callproc('sp_createUser',(_name,_email,_hashed_password))
             data = cursor.fetchall()
+
 
             if len(data) is 0:
                 conn.commit()
@@ -49,6 +66,34 @@ def signUp():
     finally:
         cursor.close()
         conn.close()
+
+@app.route('/validateLogin',methods=['POST'])
+def validateLogin():
+    try:
+        _username = request.form['inputEmail']
+        _password = request.form['inputPassword']
+
+
+        con = mysql.connect()
+        cursor = con.cursor()
+        cursor.callproc('sp_validateLogin',(_username,))
+        data = cursor.fetchall()
+
+        if len(data) > 0:
+            if check_password_hash(str(data[0][3]),_password):
+                session['user'] = data[0][0]
+                return redirect('/userHome')
+            else:
+                return render_template('error.html',error = 'Wrong Email address or Password.')
+        else:
+            return render_template('error.html',error = 'Wrong Email address or Password.')
+
+
+    except Exception as e:
+        return render_template('error.html',error = str(e))
+    finally:
+        cursor.close()
+        con.close()
 
 if __name__ == "__main__":
     app.run()
